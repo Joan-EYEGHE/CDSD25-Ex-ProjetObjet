e1be9e2be0edf40c96e58e6c8cbf5071bf4743e3
EYEGHE Joan <joanarcher26@gmail.com>
2025-07-04 21:35:24 +0000

    Gérer le metier // Dans le dossier metier Création de la classe Traitement Dans la classe Traitement // Faire des calculs sur les objets : le total et le pourcentage

--- src/metier/Traitement.java
+++ src/metier/Traitement.java
@@ -1,5 +1,29 @@
 package metier;
 
+import dao.BD;
+import domaine.*;
+import java.util.ArrayList;
+
 public class Traitement {
 
+    private BD bd=new BD();
+
+    // Gérer les calculs
+    private void statistiques(){
+        ArrayList<Enseignant> listeens=bd.obtenirEnseignants();
+        int ctv=0; // nombre de vacataires
+        int ctp=0; // nombre de Permanents
+        for (Enseignant e : listeens){
+            if (e instanceof Permanent){
+                ctp++;
+            }else {
+                ctv++;
+            }
+        }
+
+        System.out.println("Nombre de vacataires : "+ ctv );
+        System.out.println("Nombre de permanents : " +ctp);
+        System.out.println("Pourcentage de vacataires : " +((ctv)/(ctv+ctp)));
+        System.out.println("Pourcentage de permanents : " +((ctp)/(ctv+ctp)));
+    }
 }
